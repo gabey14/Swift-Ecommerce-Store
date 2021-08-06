661b2fe79853f187a73b35b88cc1bc305189f12f
gabey14 <abeygeorge14@gmail.com>
2021-08-06 18:48:34 +0530

    completed stripe checkout

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-msmufxvw.us.auth0.com'
-    clientId='9Mx8cZzwx3mkgLNmqMUMceFE2CBUUoDE'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
